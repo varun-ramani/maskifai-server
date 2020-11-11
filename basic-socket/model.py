1699109e5b19b102d2502d149e367e0b3cd51412
Simon Chervenak <sarras305@gmail.com>
2020-11-11 13:17:19 -0500

    add tester

---
+++ basic-socket/model.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+model=load_model("../model2-001.model")
+
+labels_dict={0:'without mask',1:'mask'}
+color_dict={0:(0,0,255),1:(0,255,0)}
+
+size = 4
+# webcam = cv2.VideoCapture(0) #Use camera 0
+
+# We load the xml file
+classifier = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
+
+def test(data):
+    # convert the raw image data to a cv2 image
+    arr = np.fromstring(data, dtype='uint8')
+    im = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
+
+    # Resize the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
+
+    # detect MultiScale / faces
+    faces = classifier.detectMultiScale(mini)
+
+    labels = []
+    
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+        #Save just the rectangle faces in SubRecFaces
+        face_img = im[y:y+h, x:x+w]
+        resized=cv2.resize(face_img,(150,150))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,150,150,3))
+        reshaped = np.vstack([reshaped])
+        result=model.predict(reshaped)
+        #print(result)
+
+        label=np.argmax(result,axis=1)[0]
+
+        labels.append(label)
+
+        # add a rectangle (todo: send this back to maskif?)
+        # cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        # cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        # cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+
+    print(labels)
+
+    return labels
--- basic-socket/server.py
+++ basic-socket/server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+import model
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @socketio.on('image')
 def handle_message(message):
-    open("message.png", "wb").write(message)
+    if any(model.test(message)):
+        socket.emit('antimasker')
 
 if __name__ == '__main__':
     socketio.run(app)
